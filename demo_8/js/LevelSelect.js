6c5231de7ab7bd0c6d344cbfa73ad296ebb376be
Sameedha Bairagi <sameedhabairagi.04@gmail.com>
2017-03-23 17:07:21 -0400

    Demo 8 added

---
+++ demo_8/js/LevelSelect.js
@@ -0,0 +1,257 @@
+// https://github.com/BdR76/phaserlevelselect
+
+var DinoEggs = DinoEggs || {}; 
+
+
+DinoEggs.LevelSelect = function(){
+    Phaser.State.call(this);
+    
+    DinoEggs.PLAYER_DATA = null;
+	this.levelIcons = [];
+};
+
+DinoEggs.LevelSelect.prototype = Object.create(Phaser.State.prototype);
+DinoEggs.LevelSelect.prototype.constructor = DinoEggs.LevelSelect;
+
+
+DinoEggs.LevelSelect.prototype = {
+
+	preload: function() {
+        //this.game.load.spritesheet('levelselecticons', 'assets/levelselecticons.png', 96, 96);
+		this.game.load.spritesheet('levelIcons', 'assets/levelIcons/level_icons.png', 73.83, 82);		
+		this.initLevelData();
+	},
+
+	create: function() {
+        this.background = this.game.add.tileSprite(0, 0, this.game.width, this.game.height, 'sky');
+		//this.game.stage.backgroundColor = 0x80a0ff;
+        this.game.add.text(256, 24,'Select a level', {font:"48px Arial"});
+
+		this.createLevelIcons();
+		this.animateLevelIcons();
+        
+        document.getElementById("eq-match-div").style.display="block";
+        document.getElementById("eq-solve-div").style.display="none";
+        
+        this.selectedLevel = -1;
+        
+        //-----------------------------------
+        optionsGroup = this.game.add.group();
+		optionsGroup.x = this.game.width*0.5;
+		optionsGroup.y = this.game.height*0.5;
+        optionsGroup.setAll('anchor.x', 0.5);
+	    optionsGroup.setAll('anchor.y', 0.5);
+		optionsGroup.scale.setTo(0,0);
+        optionsBg = this.game.add.sprite(0,0, "options");
+		optionsBg.anchor.setTo(0.5,0.5);
+        	
+		msg = this.game.add.text(optionsBg.x-150, optionsBg.y+50,'Choose an option', { fontSize: '25px', fill: '#000' });
+		
+		tutorial = this.game.add.sprite(optionsBg.x-150,msg.y+20, "tutorial");
+        tutorial.scale.setTo(0.7,0.7);     
+        tutorial.alpha = 0.8;
+		tutorial.inputEnabled = true;
+		tutorial.events.onInputDown.add(this.tut_listener,this);
+        tutorial.events.onInputOver.add(this.over, this);
+	    tutorial.events.onInputOut.add(this.out, this);
+		
+		play = this.game.add.sprite(optionsBg.x+50,msg.y+20, "play");
+        play.scale.setTo(0.7,0.7);     
+        play.alpha = 0.8;
+		play.inputEnabled = true;
+		play.events.onInputDown.add(this.play_listener,this);
+        play.events.onInputOver.add(this.over, this);
+	    play.events.onInputOut.add(this.out, this);
+        
+        exit = this.game.add.sprite(optionsBg.x-200,optionsBg.y+10, "exit");
+        exit.scale.setTo(0.7,0.7);        
+		exit.inputEnabled = true;
+        exit.events.onInputDown.add(this.exit_listener,this);
+        exit.alpha = 0.8;
+        exit.events.onInputOver.add(this.over, this);
+	    exit.events.onInputOut.add(this.out, this);
+        
+        optionsGroup.add(optionsBg);
+        optionsGroup.add(msg);
+        optionsGroup.add(tutorial);
+        optionsGroup.add(play);
+        optionsGroup.add(exit);
+        //---------------------------------------------
+	},
+
+	update: function() {
+	},
+	
+	initLevelData: function() {
+
+		if (!DinoEggs.PLAYER_DATA) {
+			var str = window.localStorage.getItem('DinoGame_Progress');
+			try {
+				DinoEggs.PLAYER_DATA = JSON.parse(str);
+			} catch(e){
+				DinoEggs.PLAYER_DATA = [];
+			};
+			if (Object.prototype.toString.call( DinoEggs.PLAYER_DATA ) !== '[object Array]' ) {
+				DinoEggs.PLAYER_DATA = [];
+			};
+		};
+	},
+
+	createLevelIcons: function() {
+		var levelNumber = 0;
+
+		for (var y=0; y < 3; y++) {
+			for (var x=0; x < 4; x++) {
+				// next level
+				levelNumber = levelNumber + 1;
+				
+				// check if array not yet initialised
+				if (typeof DinoEggs.PLAYER_DATA[levelNumber-1] !== 'number') {
+					if (levelNumber == 1) {
+						DinoEggs.PLAYER_DATA[levelNumber-1] = 0; // level 1 should never be locked
+					} else {
+						DinoEggs.PLAYER_DATA[levelNumber-1] = -1;
+					};
+				};
+
+				// player progress info for this level
+				var playdata = DinoEggs.PLAYER_DATA[levelNumber-1];
+
+				// decide which icon
+				var isLocked = true; // locked
+				var stars = 0; // no stars
+				
+				// check if level is unlocked
+				if (playdata > -1) {
+					isLocked = false; // unlocked
+					if (playdata < 4) {stars = playdata;}; // 0 to 3 stars
+				};
+
+				// calculate position on screen
+				var xpos = 160 + (x*128);
+				var ypos = 120 + (y*128);
+				
+				// create icon
+				this.levelIcons[levelNumber-1] = this.createLevelIcon(xpos, ypos, levelNumber, isLocked, stars);
+				var backicon = this.levelIcons[levelNumber-1].getAt(0);
+
+				// keep levelNumber, used in onclick method
+				backicon.health = levelNumber;
+
+				// input handler
+				backicon.inputEnabled = true;
+				backicon.events.onInputDown.add(this.onSpriteDown, this);
+                backicon.input.useHandCursor = true;
+			};
+		};
+	},
+
+	// -------------------------------------
+	// Add level icon buttons
+	// -------------------------------------
+	createLevelIcon: function(xpos, ypos, levelNumber, isLocked, stars) {
+
+		// create new group
+		var IconGroup = this.game.add.group();
+		IconGroup.x = xpos;
+		IconGroup.y = ypos;
+
+		// keep original position, for restoring after certain tweens
+		IconGroup.xOrg = xpos;
+		IconGroup.yOrg = ypos;
+
+		// determine background frame
+		var frame = 0;
+		if (isLocked == false) {frame = 1};
+		
+		// add background
+		var icon1 = this.game.add.sprite(0, 0, 'levelIcons', frame);
+		IconGroup.add(icon1);
+
+		// add stars, if needed
+		if (isLocked == false) {
+            var txt = this.game.add.text(24, 16,''+levelNumber, {font:"45px Arial"});
+			var icon2 = this.game.add.sprite(0, 0, 'levelIcons', (2+stars));
+			
+			IconGroup.add(txt);
+			IconGroup.add(icon2);
+		};
+		
+		return IconGroup;
+	},
+
+	onSpriteDown: function(sprite, pointer) {
+
+		// retrieve the iconlevel
+		var levelNumber = sprite.health;
+
+		if (DinoEggs.PLAYER_DATA[levelNumber-1] < 0) {
+			// indicate it's locked by shaking left/right
+			var IconGroup = this.levelIcons[levelNumber-1];
+			var xpos = IconGroup.xOrg;
+
+			var tween = this.game.add.tween(IconGroup)
+				.to({ x: xpos+6 }, 20, Phaser.Easing.Linear.None)
+				.to({ x: xpos-5 }, 20, Phaser.Easing.Linear.None)
+				.to({ x: xpos+4 }, 20, Phaser.Easing.Linear.None)
+				.to({ x: xpos-3 }, 20, Phaser.Easing.Linear.None)
+				.to({ x: xpos+2 }, 20, Phaser.Easing.Linear.None)
+				.to({ x: xpos }, 20, Phaser.Easing.Linear.None)
+				.start();
+		} else {
+			// simulate button press animation to indicate selection
+			var IconGroup = this.levelIcons[levelNumber-1];
+			var tween = this.game.add.tween(IconGroup.scale)
+				.to({ x: 0.9, y: 0.9}, 100, Phaser.Easing.Linear.None)
+				.to({ x: 1.0, y: 1.0}, 100, Phaser.Easing.Linear.None)
+				.start();
+				
+			// it's a little tricky to pass selected levelNumber to callback function, but this works:
+			tween.onComplete.add(function(){this.onLevelSelected(sprite.health);}, this);
+		};
+	},
+
+	animateLevelIcons: function() {
+
+		// slide all icons into screen
+		for (var i=0; i < this.levelIcons.length; i++) {
+			// get variables
+			var IconGroup = this.levelIcons[i];
+			IconGroup.y = IconGroup.y + 600;
+			var y = IconGroup.y;
+
+			// tween animation
+			this.game.add.tween(IconGroup).to( {y: y-600}, 500, Phaser.Easing.Back.Out, true, (i*40));
+		};
+	},
+
+	onLevelSelected: function(levelNumber) {
+        this.game.add.tween(optionsGroup.scale).to({ x: 1,y:1}, 500,  Phaser.Easing.Bounce.Out,true);
+        this.selectedLevel=levelNumber;
+	},
+    tut_listener: function(){
+        $('#tutorialModal').modal('show');
+        $('#tFrame').contents().find('.levelTutorial').hide()
+        $('#tFrame').contents().find("#"+this.selectedLevel).show();
+        document.getElementById("tFrame").contentWindow.g_done_count = 0;
+        console.log("done count ", document.getElementById("tFrame").contentWindow.g_done_count);
+
+    },
+    play_listener: function(){
+        switch(this.selectedLevel){
+            case 1: this.state.start('Level1'); break;
+            case 2: this.state.start('Level2'); break;
+            case 3: this.state.start('Game'); break;
+        }
+    },
+    exit_listener: function(){
+        this.game.add.tween(optionsGroup.scale).to({ x: 0,y:0}, 500,  Phaser.Easing.Bounce.Out,true);
+    },
+    over: function(item) {
+			item.alpha=1;
+    },
+    out: function(item) {
+            item.alpha=0.8;
+    }
+    
+};
---
+++ demo_8/js/gamePieces.js
@@ -0,0 +1,75 @@
+//------------------------------------------------------
+Rock = function (game, x, y, equation) {
+
+    Phaser.Sprite.call(this, game, x, y, 'rock');
+    
+    this.game.physics.arcade.enable(this);
+    //set velocity in game while spawning rock
+    this.body.velocity.y = 15;
+    this.body.collideWorldBounds = true;
+    this.equ = equation;
+    
+    //Add equation text on rock sprite. TO DO: change font size when sprite size is altered
+    var text = this.game.add.text(Math.floor( this.width / 2), Math.floor(this.height / 2), this.equ, { font: "25px Comic Sans MS", fill: "#ff0044", wordWrap: true, wordWrapWidth: this.width, align: "center"});
+    text.anchor.set(0.5);
+    this.equationText = text;
+    //this.addChild(text);    
+
+};
+
+Rock.prototype = Object.create(Phaser.Sprite.prototype);
+Rock.prototype.constructor = Rock;
+
+Rock.prototype.getEquation = function(){
+    return this.equ;
+}
+Rock.prototype.setEquation = function(equation){
+    this.equ = equation;
+    //Add equation text on rock sprite. TO DO: change font size when sprite size is altered
+    var text = this.game.add.text(Math.floor( this.width / 2), Math.floor(this.height / 2), this.equ, { font: "25px Comic Sans MS", fill: "#ff0044", wordWrap: true, wordWrapWidth: this.width, align: "center"});
+    text.anchor.set(0.5);
+    this.equationText = text;
+}
+
+
+//.......................................................
+
+//-------------------------------------------------------
+Egg = function (game, x, y, equation) {
+
+    Phaser.Sprite.call(this, game, x, y, 'egg');
+    this.game.physics.arcade.enable(this);
+    this.body.gravity.y = 300;
+    this.body.bounce.y =  0.5; 
+    this.body.collideWorldBounds = true;
+    
+    this.equ = equation;
+    
+    var text = this.game.add.text(Math.floor(this.width / 2), Math.floor(this.height / 2), this.equ, {font: "20px Comic Sans MS", fill: "#111111",wordWrap: true, wordWrapWidth: this.width, align: "center"});
+    text.anchor.set(0.5);
+    
+    this.equationText = text;
+    
+    this.hitCounter=0;
+    
+    //add click event to egg
+    this.inputEnabled = true;
+    //this.events.onInputDown.add(populateSolveEqCanvas, this, this);
+    
+    this.animations.add('hatch',['egg.png','hatch1.png','hatch2.png','hatch3.png','hatch4.png','hatch5.png','hatch6.png','hatch7.png','hatch8.png','hatch9.png','hatch10.png','hatch11.png','hatch12.png','hatch13.png','hatch14.png'],6,false);
+    this.animations.add('wiggleOnce',['wiggle1.png','wiggle2.png','wiggle3.png','wiggle2.png','egg.png','wiggle4.png','wiggle5.png','wiggle4.png','egg.png'],10,false);
+    this.animations.add('wiggleContinous',['wiggle1.png','wiggle2.png','egg.png','wiggle4.png','egg.png'],4,true);
+
+};
+
+Egg.prototype = Object.create(Phaser.Sprite.prototype);
+Egg.prototype.constructor = Egg;
+//                 ___________________
+Egg.prototype.getEquation = function(){
+    return this.equ;
+}
+
+Egg.prototype.setEquStyle = function(style){
+    this.children.forEach(function(c){ c.setStyle(style)});
+}
+
