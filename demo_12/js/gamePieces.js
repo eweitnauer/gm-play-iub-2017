5198f9d581b62c44938a2cea29d79a578def0deb
Shree Harsha Sridharamurthy <shreeharshas@users.noreply.github.com>
2017-04-23 16:38:26 -0400

    Update gamePieces.js

--- demo_12/js/gamePieces.js
+++ demo_12/js/gamePieces.js
@@ -71,7 +71,7 @@ Egg = function (game, x, y, equation, solutions) {
     this.inputEnabled = true;
     //this.events.onInputDown.add(populateSolveEqCanvas, this, this);
     
-    this.animations.add('hatch',['egg.png','hatch1.png','hatch2.png','hatch3.png','hatch4.png','hatch5.png','hatch6.png','hatch7.png','hatch8.png','hatch9.png','hatch10.png','hatch11.png'],6,false);
+    this.animations.add('hatch',['egg.png','hatch1.png','hatch2.png','hatch3.png','hatch4.png','hatch5.png','hatch6.png','hatch7.png','hatch8.png','hatch9.png','hatch10.png','hatch11.png','hatch12.png','hatch13.png','hatch14.png'],6,false);
     this.animations.add('wiggleOnce',['wiggle1.png','wiggle2.png','wiggle3.png','wiggle2.png','egg.png','wiggle4.png','wiggle5.png','wiggle4.png','egg.png'],10,false);
     this.animations.add('wiggleContinous',['wiggle1.png','wiggle2.png','egg.png','wiggle4.png','egg.png'],4,true);
     this.canvasId = "gseq_0";
